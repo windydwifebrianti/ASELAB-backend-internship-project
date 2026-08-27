2eab16410809aa6910399f4445e1291931c612af
windydwifebrianti <windydf25@gmail.com>
2026-08-27 19:52:52 +0700

    update rute untuk login

--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { register } from "../controllers/authController";
+import { register, login } from "../controllers/authController";
 
 const router = Router();
 
 // Ketika ada yang menembak POST ke /register, jalankan fungsi register
 router.post("/register", register);
+router.post("/login", login);
 
 export default router;
