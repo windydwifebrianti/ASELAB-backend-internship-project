b2fae8f1a1a712092f2c9d51857b76a995eb485d
Damar Syahada <damarsyahada12@gmail.com>
2026-09-25 07:00:28 +0700

    refactor: change hardcoded port

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import teamRoutes from "./routes/teamRoutes";
 import matchRoutes from "./routes/matchRoutes";
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors());
